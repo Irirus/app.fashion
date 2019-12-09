b054225103dc2efb8d61da2854a54e1e00c78f98
bluestara <58248395+bluestara@users.noreply.github.com>
2019-12-09 16:32:13 +0700

    Revert "Revert "oke la""
    This reverts commit c08a582a3c97949a2c9bdccbb7826ffef3ab2914.

--- app/src/main/java/com/example/nomorepleze/AccountActivity.java
+++ app/src/main/java/com/example/nomorepleze/AccountActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,6 +32,7 @@ public class AccountActivity extends AppCompatActivity {
                 mAuth.signOut();
                 //Intent home = new Intent(AccountActivity.this, HomeActivity.class);
                 //startActivity(home);
+                Toast.makeText(AccountActivity.this, "Remember vote for us 5 stars ^^", Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
